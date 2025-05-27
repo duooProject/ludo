91a93eb954a14f9594f8d9dcf0ff4519c76c5577
Fawzi Mouelhi <mouelhiam23@gmail.com>
2025-05-27 15:05:29 +0100

    updated game js

--- js/components/game.js
+++ js/components/game.js
@@ -1,3 +1,10 @@
+function Player(){
+  return {
+  hand:[],
+  myCard:[],
+  score:0,
+  }
+}
 
 function startGame() {
 
@@ -5,6 +12,10 @@ function startGame() {
 
 function Game(playerCount) {
   return {
+    deck:[],
+    table:[],
+    players:[],
+    round:3,
     startGame: startGame,
     resetGame: resetGame,
   };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 function onStartGame(){
-  var table=component("div")
-  table.addClass("gameTable")
-   var body = document.querySelector("body");
-   body.appendChild(table.renderComponent());
-  
-
 
 }
 
